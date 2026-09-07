bb15599410a52e3d260569b96fa66b5d79273a97
Zavyar Tajdar <zavyar.tajdar100@gmail.com>
2026-09-07 16:05:02 +0500

    Add new routes for managing saved content: implement endpoints to retrieve saved content, specific collections by name, and all collections for a user. Include functionality to delete collections by name, enhancing content management capabilities.

--- src/Routes/saved.routes.ts
+++ src/Routes/saved.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { saveContent, unsaveContent } from "../Controllers/saved.controller";
+import { deleteSavedCollectionByName, getSavedContentByUserIdController, getSavedCollectionByName, getSavedContent, saveContent, unsaveContent } from "../Controllers/saved.controller";
 import { verifyJWT } from "../Middleware/auth.middleware";
 
 const router = Router();
@@ -9,5 +9,33 @@ router.post("/save/reel/:reelId", verifyJWT, saveContent);
 
 router.delete("/unsave/post/:postId", verifyJWT, unsaveContent);
 router.delete("/unsave/reel/:reelId", verifyJWT, unsaveContent);
+
+// Get saved content from a specific collection
+router.get(
+    "/saved",
+    verifyJWT,
+    getSavedContent
+);
+
+// Get a specific saved collection by name
+router.get(
+    "/saved/collection/:collectionName",
+    verifyJWT,
+    getSavedCollectionByName
+);
+
+// Get all saved collections of logged-in user
+router.get(
+    "/saved/user",
+    verifyJWT,
+    getSavedContentByUserIdController
+);
+
+// Delete saved collection by name
+router.delete(
+    "/saved/collection/:collectionName",
+    verifyJWT,
+    deleteSavedCollectionByName
+);
 
 export { router as savedRoutes };
